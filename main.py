2936bd4b09850e0e8ab116b21b1e225782cd6992
Juanse <jsrvjuan@gmail.com>
2024-09-12 22:46:35 -0500

    algoritmo dijkstra implementado

--- main.py
+++ main.py
@@ -1,19 +1,30 @@
+from algorithm import aStar, best, dijkstra, neighbors
 
-from algorithm import aStar, best, neighbors
-
+# Definir el nodo inicial y el nodo objetivo
 ini = 'Tauramena'
 goal = 'Bogotá'
-graph = neighbors.getGraph()
-path = aStar.aStar(graph,ini,goal,neighbors.getPosition())
 
-if path:
-    print("Path A* found:", path)
+# Obtener el grafo y las posiciones desde neighbors.py
+graph = neighbors.getGraph()
+position = neighbors.getPosition()
+
+# Ejecutar A* y mostrar el resultado
+path_aStar = aStar.aStar(graph, ini, goal, position)
+if path_aStar:
+    print("Path A* found:", path_aStar)
 else:
     print("No path found between", ini, "and", goal)
 
-path = best.greedyBestFirstSearch(graph,ini,goal,neighbors.getPosition())
-
-if path:
-    print("Path Greedy found:", path)
+# Ejecutar Greedy Best-First Search y mostrar el resultado
+path_best = best.greedyBestFirstSearch(graph, ini, goal, position)
+if path_best:
+    print("Path Greedy found:", path_best)
 else:
-    print("No path found between", ini, "and", goal)   
+    print("No path found between", ini, "and", goal)
+
+# Ejecutar Dijkstra y mostrar el resultado
+path_dijkstra = dijkstra.dijkstra(graph, ini, goal)
+if path_dijkstra:
+    print("Path Dijkstra found:", path_dijkstra)
+else:
+    print("No path found between", ini, "and", goal)
